cb25fdadd4f0cbea010432b3f6c551a243b74b16
WizVader <126302304+WizVader@users.noreply.github.com>
2024-07-20 21:33:07 +1200

    Task Table Migration

---
+++ msa-project.Server/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿
+public class Task
+{
+    public int Id { get; set; }
+
+    public string Label { get; set; }
+
+    public string Content { get; set; }
+    public string UserEmail { get; set; }
+}
--- msa-project.Server/Services/DummyEmailSender.cs
+++ msa-project.Server/Services/DummyEmailSender.cs
@@ -5,10 +5,10 @@ namespace msa_project.Server.Services
 {
     public class DummyEmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public System.Threading.Tasks.Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             // No implementation needed, it's a dummy sender to satisfy the dependency
-            return Task.CompletedTask;
+            return System.Threading.Tasks.Task.CompletedTask;
         }
     }
 }
